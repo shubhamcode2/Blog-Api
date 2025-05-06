9f9c638728a64d6272b3c9e6f3bb9d4154fe8c6e
shubham singhk <shubhamsinghia160@gmail.com>
2025-05-06 19:05:32 +0530

    check

--- src/routes/postRoute.js
+++ src/routes/postRoute.js
@@ -12,7 +12,8 @@ const postRouter = express.Router();
 
 postRouter.route("/create").post(authenticateUser, authorizeRoles("admin", "editor"), upload.single("postPicture"), createPost);
 
-postRouter.route("/get-all-posts").get(authenticateUser, getAllPosts);
+// postRouter.route("/get-all-posts").get(authenticateUser, getAllPosts);
+postRouter.route("/get-all-posts").get( getAllPosts);
 
 postRouter.route("/getSinglePost/:id").get(authenticateUser, getSinglePost);
 
